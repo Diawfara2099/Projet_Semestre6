56ee899674a42a81cc506db611d37617de4f45f7
Diawfara <diawibrahima313@gmail.com>
2024-07-18 18:47:53 +0000

    Message de commit descriptif ici

--- actions/auth/loginAction.php
+++ actions/auth/loginAction.php
@@ -3,15 +3,15 @@ require('../../Database/utilisateur_db.php');
 
 if (isset($_POST['send'])) {
     if (!empty($_POST['email']) && !empty($_POST['password'])) {
-        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            $email = htmlspecialchars($_POST['email']);
-            $password = htmlspecialchars($_POST['password']);
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $check = getUserByEmail($email);
 
             if ($check->rowCount() > 0) {
                 $userInfos = $check->fetch();
-                if ($password === $userInfos['password']) { // Direct comparison without hashing
+                if (password_verify($password, $userInfos['password'])) {
                     session_start();
                     $_SESSION['auth'] = true;
                     $_SESSION['id'] = $userInfos['id'];
@@ -20,6 +20,7 @@ if (isset($_POST['send'])) {
                     $_SESSION['email'] = $userInfos['email'];
                     $_SESSION['type'] = $userInfos['type'];
                     header('Location: ../../index.php');
+                    exit();
                 } else {
                     $errorMessage = "Mot de passe incorrect !";
                 }
