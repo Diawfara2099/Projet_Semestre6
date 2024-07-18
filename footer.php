7b2763d5485e86d4eb68a030718e13ee6e01e66d
Diawfara <diawibrahima313@gmail.com>
2024-07-18 18:05:24 +0000

    Message de commit descriptif ici

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer mt-auto py-3 bg-body-tertiary">
   <div class="container">
-    <span class="text-body-secondary">@ GROUPE ISI <?php echo date('Y'); ?></span>
+    <span class="text-body-secondary">@ UNCHK <?php echo date('Y'); ?></span>
   </div>
 </footer>
 <!-- <script src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
